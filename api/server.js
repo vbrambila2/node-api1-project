04e768ede361afacf5c439e15925a1897a936df3
Victor Brambila <victorbrambila22@gmail.com>
2022-04-11 13:01:16 -0700

    GET requests complete

--- api/server.js
+++ api/server.js
@@ -1,3 +1,28 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(user => {
+            res.json(user);
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    console.log(req.params);
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(user)
+            }
+        })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
