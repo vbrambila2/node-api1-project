b4a0423698b005bbc393423f124fa9804dff1325
Victor Brambila <victorbrambila22@gmail.com>
2022-04-11 13:17:47 -0700

    PUT and DELETE complete

--- api/server.js
+++ api/server.js
@@ -27,13 +27,42 @@ server.get('/api/users/:id', (req, res) => {
 server.post('/api/users', (req, res) => {
     let user = req.body;
     User.insert(user)
+        .then(user => {
+            if(!user) {
+                res.status(400).json({ message: "Please provide name and bio for the user" });
+            } else {
+                res.status(201).json(user);
+            }
+        })
+});
+
+server.put('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    let user = req.body;
+
+    User.update(id, user)
         .then(updatedUser => {
-            if(!updatedUser) {
+            if(!id) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            } else if (!user.name || !user.bio) {
                 res.status(400).json({ message: "Please provide name and bio for the user" });
             } else {
                 res.json(updatedUser);
             }
         })
-});
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+
+    User.remove(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(user)
+            }
+        })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
